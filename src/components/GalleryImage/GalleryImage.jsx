ed8fa0f6030f36aca25d342820d04e2da16e3eb4
padifa <padifa7@live.fr>
2024-09-22 21:52:47 -0500

    the page is now responsive

--- src/components/GalleryImage/GalleryImage.jsx
+++ src/components/GalleryImage/GalleryImage.jsx
@@ -1,38 +1,38 @@
 import { useState } from "react";
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button'; 
-
-
+import Button from 'react-bootstrap/Button';  
 function GalleryImage({ image, onImageLike }) {
     const [liked, setLiked] = useState(image.liked);
 const [flipped, setFlipped] = useState(false);
 
 const toggleFlip = () => {
     setFlipped(!flipped);
+};
+
+const handleLikeClick = (e) => {
+    e.stopPropagation();
+    setLiked(!liked);
+    onImageLike(image);
 }
     return (
         <Col>
             <Card onClick={toggleFlip} style={{ cursor: 'pointer'}}>
-               {!flipped ? (
-                <img src={image.url || "/images/goat_small.jpg"} alt={image.title || "Goat"} className="img-fluid"/>) : (
-                <Card.Body>
+                <img src={image.url || "/images/goat_small.jpg"} alt={image.title || "Goat"} className="img-fluid"
+                style={{ display: flipped ? 'none' : 'block' }} />
+                <Card.Body style={{ display: flipped ? 'block' : 'none' }}>
                     <Card.Title>{image.title}</Card.Title>
-                    <Card.Title>{image.description}</Card.Title>
- 
+ </Card.Body>
                     <Button 
                         className='mx-1' 
                         variant={liked ? 'success' : 'primary'} 
                         size="sm" 
-                        onClick={() => {
-                            e.stopPropagation();
-                            setLiked(!liked);
-                            onImageLike(image); 
-                        }}>
+                        onClick={handleLikeClick} 
+                        style={{ position: 'absolute', bottom: '10px', right: '10px'}}>
                         {liked ? 'Liked' : 'Like'}
                     </Button>
-                </Card.Body>
-                )}
+                
+                
             </Card>
             
         </Col>
